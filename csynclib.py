47ebf6b06530e4a4fc4e217c61444ec5e803435e
craig <craig@lasalle.yumaed.org>
2013-03-11 10:35:42 -0700

    issue #3, fix library location issues

--- csynclib.py
+++ csynclib.py
@@ -7,7 +7,7 @@ STRING = c_char_p
 _libraries = {}
 if os.path.exists('/usr/lib/libocsync.so.0'):
 	_libraries['/usr/lib/libocsync.so.0'] = CDLL('/usr/lib/libocsync.so.0')
-if os.path.exists('/usr/lib64/libocsync.so.0'):
+elif os.path.exists('/usr/lib64/libocsync.so.0'):
 	_libraries['/usr/lib/libocsync.so.0'] = CDLL('/usr/lib64/libocsync.so.0')
 else:
 	path = ctypes.util.find_library('libocsync')
