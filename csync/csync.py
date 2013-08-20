61f59d8483f4d424583d243b591f69f3e9c94b76
Lluis Gili <lluis@ingent.net>
2013-08-20 11:08:27 +0200

    don't use a default destination folder, by default sync to root folder

--- csync/csync.py
+++ csync/csync.py
@@ -333,7 +333,7 @@ Password options:
 	parser.add_argument('-s', '--src', nargs='?',
 		default =  os.path.expanduser(os.path.join('~','ownCloud')),
 		 help = "Local Directory to sync with.")
-	parser.add_argument('-d', '--dst', nargs='?', default = 'clientsync',
+	parser.add_argument('-d', '--dst', nargs='?', default = '',
 		help = "Folder on server.")
 	parser.add_argument('--url', nargs='?', default = None,
 		 help = "URL to sync to.")
